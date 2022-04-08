77277b1d534c60ae29cefcae6321191a34103f51
amarpustam45 <amarpustam@hotmail.com>
2022-04-08 15:14:18 -0400

    register users to db

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,27 @@
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+const { BadRequestError } = require('../errors/bad-request');
+const bcrypt = require('bcryptjs');
+
 const register = async (req, res) => {
-  res.send('register user');
+  // //check for if the value is present when registering
+  // const { name, email, password } = req.body;
+  // if (!name || !email || !password) {
+  //   throw new BadRequestError('Please provide a name, email and password!');
+  // }
+
+  //All of this can be setup in the schema
+  // //create new user with hashed password
+  // const { name, email, password } = req.body;
+
+  // //get random bytes to hash the password with
+  // const salt = await bcrypt.genSalt(10);
+  // const hashedPassword = await bcrypt.hash(password, salt);
+  // const tempUser = { name, email, password: hashedPassword };
+
+  //create the user using mongoose
+  const user = await User.create({ ...req.body });
+  res.status(StatusCodes.CREATED).json({ user });
 };
 
 const login = async (req, res) => {
